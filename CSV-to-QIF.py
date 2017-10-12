59e2f843377e73e0297c7759ad2a277ccc3deb3f
Steven Saus <steven@stevesaus.com>
2017-10-12 17:31:54 -0400

    Added multiple columns, mint.com example, multiple --builtin export for when there's account names, etc

--- CSV-to-QIF.py
+++ CSV-to-QIF.py
@@ -2,12 +2,14 @@
 #
 #    @file    CSV_to_QIF.py
 #    @author   Mario Avenoso (M-tech Creations)
+#    @contributor   Steven Saus (uriel1998)
 #    @license  MIT (see license.txt)
 #
 #    Program to convert from a CSV to a QIF file using a definitions
 #    to describe how the CSV is formatted
 #
 #    @section  HISTORY
+#    v0.3 - added multiple columns, multiple file export based on account name, etc
 #    v0.2 - Added payee ignore option  1/18/2016 feature update
 #    v0.1 - First release 1/1/2016 beta release
 #
@@ -30,12 +32,11 @@ import csv
 #     File with the settings for converting CSV
 #
 #
-def readCsv(inf_,outf_,deff_): #will need to receive input csv and def file
+def readCsv(inf_,deff_,tofile_): #will need to receive input csv and def file
 
     csvdeff = csv.reader(deff_, delimiter=',')
 
     next(csvdeff,None)
-
 
 
     for settings in csvdeff:
@@ -46,17 +47,29 @@ def readCsv(inf_,outf_,deff_): #will need to receive input csv and def file
         deli_ = settings[5] #How the csv is separated
         header_ = int(settings[6])  #Set if there is a header to skip
 
+        if len(settings) <= 7:  #for backward compatibility
+            sign_ = -1
+            category_ = -1
+            account_ = tofile_  #a string instead of a handle so it works with everything
+        else:
+            sign_ = int(settings[7])  #Which direction of sign
+            category_ = int(settings[8])  #Which direction of sign
+            account_ = int(settings[9])  #Which account this is
+            
+        
     csvIn = csv.reader(inf_, delimiter=deli_)  #create csv object using the given separator
-
+    
     if header_ >= 1: #If there is a header skip the fist line
         next(csvIn,None)  #skip header
-
-    for row in csvIn:
-        writeFile(row[date_],row[amount_],row[memo_],row[payee_],outf_)  #export each row as a qif entry
-
-
-
-
+    
+    if len(settings) <= 7:  #for backward compatibility
+        for row in csvIn:
+            writeFile(row[date_],row[amount_],row[memo_],row[payee_],sign_,category_,tofile_,tofile_)  #export each row as a qif entry
+    
+    else:
+    
+        for row in csvIn:
+            writeFile(row[date_],row[amount_],row[memo_],row[payee_],row[sign_],row[category_],row[account_],tofile_)  #export each row as a qif entry
 
 #
 #     @brief Receives data to be written to and its location
@@ -75,20 +88,48 @@ def readCsv(inf_,outf_,deff_): #will need to receive input csv and def file
 #
 # https://en.wikipedia.org/wiki/Quicken_Interchange_Format
 #
-def writeFile(date_,amount_,memo_,payee_, filelocation_):
-    outFile = open(filelocation_,"a")  #Open file to be appended
+
+
+# because this function opens and closes the file each call (nice!) it's
+# trivial then for me to substitute in filenames per ACCOUNT. 
+
+def writeFile(date_,amount_,memo_,payee_,sign_,category_, account_, filelocation_):
+
+    
+    if filelocation_ == "builtin":
+        suffix = '.qif'
+        tofile = os.path.join(os.getcwd(), account_ + suffix)
+    else:
+        tofile = account_
+
+
+    outFile = open(tofile,"a")  #Open file to be appended
     outFile.write("!Type:Cash\n")  #Header of transaction, Currently all set to cash
     outFile.write("D")  #Date line starts with the capital D
     outFile.write(date_)
     outFile.write("\n")
-
+    
     outFile.write("T")  #Transaction amount starts here
-    outFile.write(amount_)
+    if(sign_!=-1):
+        if sign_ in ('debit', 'DEBIT'):           
+            aamount_ = 0 - float(amount_)
+            outFile.write(str(aamount_))
+        elif sign_ in ('credit', 'CREDIT'):           
+            outFile.write(amount_)
+    else:    
+        outFile.write(amount_)
+    
     outFile.write("\n")
 
     outFile.write("M")  #Memo Line
     outFile.write(memo_)
     outFile.write("\n")
+
+    if(category_!=-1):   #for backward compatibility
+        outFile.write("L")  #Category
+        outFile.write(category_)
+        outFile.write("\n")    
+
 
     if(payee_!=-1):
         outFile.write("P")  #Payee line
@@ -97,42 +138,58 @@ def writeFile(date_,amount_,memo_,payee_, filelocation_):
 
     outFile.write("^\n")  #The last line of each transaction starts with a Caret to mark the end
     outFile.close()
+
 def convert():
 
+    error = 'Input error!____ Format [import.csv] [output.csv] [import.def] ____\n\n\
+            OR -builtin [import.csv] [import.def]\n\
+            [import.csv] = File to be converted\n\
+            [output.qif] = File to be created OR switch\n\
+            [import.def] = Definition file describing csv file\n'
 
-     error = 'Input error!____ Format [import.csv] [output.csv] [import.def] ____\n\n\
-                 [import.csv] = File to be converted\n\
-                 [output.qif] = File to be created\n\
-                 [import.def] = Definition file describing csv file\n'
+    if (len(sys.argv) != 4):  #Check to make sure all the parameters are there
+        print error
+        exit(1)
 
-     if (len(sys.argv) != 4):  #Check to make sure all the parameters are there
-            print error
+    if sys.argv[1].lower() == "--builtin":   # for backward compatibility
+        tofile = "builtin"
+
+        if os.path.isfile(sys.argv[2]):
+            fromfile = open(sys.argv[2],'r')
+        else:
+            print '\nInput error!____ import.csv: ' + sys.argv[2] + ' does not exist / cannot be opened !!\n'
+            exit(1) 
+        if os.path.isfile(sys.argv[3]):
+            deffile = open(sys.argv[3],'r')
+        else:
+            print '\nInput error!____ import.def: ' + sys.argv[3] + ' does not exist / cannot be opened !!\n'
+            exit(1)
+    else:
+        
+        if os.path.isfile(sys.argv[1]):
+            fromfile = open(sys.argv[1],'r')
+        else:
+            print '\nInput error!____ import.csv: ' + sys.argv[1] + ' does not exist / cannot be opened !!\n'
+            exit(1)
+            
+        try:
+            tofile   = open(sys.argv[2],'a')
+        except:
+            print '\nInput error!____ output.csv: ' + sys.argv[2] + ' cannot be created !!\n'
             exit(1)
 
-     if os.path.isfile(sys.argv[1]):
-         fromfile = open(sys.argv[1],'r')
-     else:
-         print '\nInput error!____ import.csv: ' + sys.argv[1] + ' does not exist / cannot be opened !!\n'
-         exit(1)
+        tofile.close()
+        tofile = sys.argv[2]   # changed this from a file handle to a string for backward compatibility.
+        
+        if os.path.isfile(sys.argv[3]):
+            deffile = open(sys.argv[3],'r')
+        else:
+            print '\nIDDDnput error!____ import.def: ' + sys.argv[3] + ' does not exist / cannot be opened !!\n'
+            exit(1)
+        
+    readCsv(fromfile,deffile,tofile)
 
-     try:
-         tofile   = open(sys.argv[2],'a')
-     except:
-         print '\nInput error!____ output.csv: ' + sys.argv[2] + ' cannot be created !!\n'
-         exit(1)
-
-     if os.path.isfile(sys.argv[3]):
-         deffile = open(sys.argv[3],'r')
-     else:
-         print '\nInput error!____ import.def: ' + sys.argv[3] + ' does not exist / cannot be opened !!\n'
-         exit(1)
-
-     tofile = sys.argv[2]
-     readCsv(fromfile,tofile,deffile)
-
-     fromfile.close()
-     deffile.close()
-
-
+    fromfile.close()
+    deffile.close()
 
 convert()#Start
--- README.md
+++ README.md
@@ -1,44 +1,44 @@
 #CSV to QIF Converter#
 
-This is a Python program to take a CSV file and convert it to a QIF file. QIF is an 
-open specification for reading and writing financial data. This is a working Beta release.
-Find my wirte up Here
+This is a Python program to take a CSV file and convert it to a QIF file. QIF is an open specification for reading and writing financial data. This is a working Beta release.
+
+Find my write up Here
     ------> http://mario.mtechcreations.com/programing/csv-to-qif-python-converter/
 
 ## About this Program ##
 
-The way you use the converter is by passing it a file to convert, the file name and path
-to convert to and the definition file describing the csv file. 
+The way you use the converter is by passing it a file to convert, the file name and path to convert to and the definition file describing the csv file. 
 
 ## Exaple Usage and File ##
 
-Example of converting a file stored in the same directory as the program.
+Example of converting a file stored in the same directory as the program:
 
-python CSV-to-QIF.py [file-to-convert.csv] [export-file.qif] [csv-discription.def]
+```python CSV-to-QIF.py [file-to-convert.csv] [export-file.qif] [csv-discription.def]
+
+**OR**
+
+```python CSV-to-QIF.py -builtin [file-to-convert.csv] [csv-discription.def]
 
 The definition file is a csv that is setup with the properties of the csv to convert.
 The format is as follows (also see the example .def file)
 
-Date,Date Format,Amount,Memo,Payee,Separator,Header
-0,not used,4,1,2,",",0
+```Date,Date Format,Amount,Memo,Payee,Separator,Header,Sign,Category,Account
+```0,not used,3,2,1,",",1,4,5,6
 
-The first row, or header, lists the type of information the converter is going to use to create the QIF file. 
-The second row has the cell number (starting at 0) of the requested data from the CSV file to be converted. 
-Meaning if the transaction amount is in the first cell you would put a 0 under amount. 
+(This is the format used for Mint.com exports.)
 
-Currently the date format cell does not do anything, the means that in order for the QIF to be valid, the date in 
-the CSV needs to be in MM/DD/YYYY format. This is a feature I hope to implement in the future.
-
-The Payee cell is used to mark the Payeee. If there is no Payee use -1 to ignore it.
-
-There are two other cell’s to make note of, Separator and Header.  Separator refers to how the file is separated. 
-CSV files can be split in different ways,  with the two most common being a semicolon or a comma. 
-In this field place the type of Separator used (ie. “;”). The header cell is used to tell the program 
-if it needs to skip the first row or not. Most times this first row just holds the names of each column and 
-needs to be skipped. If so Place a 1 in the cell, otherwise place a 0.
+* The first row, or header, lists the type of information the converter is going to use to create the QIF file.  
+* The second row has the cell number (starting at 0) of the requested data from the CSV file to be converted.  That means that if the transaction amount is in the first cell you would put a 0 in that row. 
+* Currently the date format cell does not do anything, the means that in order for the QIF to be valid, the date in the CSV needs to be in MM/DD/YYYY format. This is a feature I hope to implement in the future.  
+* The Payee cell is used to mark the Payeee. If there is no Payee use -1 to ignore it.  
+* The Sign cell is used to mark the debit/credit nature of the transaction if it is not reflected by the account (e.g. Mint).  If the sign of the transaction is incorporated in the Amount cell, then use -1 to ignore it.
+* The Account cell is used to mark which account is being used and will create export names if the -builtin tag is used.
+* Separator refers to how the file is separated. CSV files can be split in different ways,  with the two most common being a semicolon or a comma. In this field place the type of Separator used (ie. “;”). 
+* The header cell is used to tell the program if it needs to skip the first row or not. Most times this first row just holds the names of each column and needs to be skipped. If so Place a 1 in the cell, otherwise place a 0.
 
 
 Written by Mario Avenoso of mtechcreations.com
+Code added by Steven Saus for sign and multi-account import/export
 MIT license, check license.txt for more information
 
 
